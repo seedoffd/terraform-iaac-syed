b66e0aa7c22237b537c8f2187a16a6c7c30fc0f9
Cloud User <centos@ip-172-31-87-249.ec2.internal>
2020-03-10 23:57:28 +0000

    subnet updated

--- vpc/output.tf
+++ vpc/output.tf
@@ -1,3 +1,27 @@
-resource "aws_vpc" "dev" { 
-  cidr_block = "10.0.0.0/16" 
-} 
+output "VPC_ID" {
+  value = "${aws_vpc.dev.id}"
+}
+
+output "Subnet1" {
+  value = "${aws_subnet.dev1.id}"
+}
+
+output "Subnet2" {
+  value = "${aws_subnet.dev2.id}"
+}
+
+output "Subnet3" {
+  value = "${aws_subnet.dev3.id}"
+}
+
+output "private subnet2" {
+  value = "${aws_vpc.dev_private1.id}"
+}
+
+output "private subnet2" {
+  value = "${aws_vpc.dev_private2.id}"
+}
+
+output "private subnet2" {
+  value = "${aws_vpc.dev_private3.id}"
+}
