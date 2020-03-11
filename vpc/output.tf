bc2e57335bfd65d4b95adef5a520637427d4f3a0
Cloud User <centos@ip-172-31-87-249.ec2.internal>
2020-03-11 00:32:55 +0000

    tfvars added

--- vpc/output.tf
+++ vpc/output.tf
@@ -1,31 +1,24 @@
 output "VPC_ID" {
-  value = "${aws_vpc.dev.id}"
+  value       = "${aws_vpc.dev.id}"
 }
-
-output "Subnet1" {
-  value = "${aws_subnet.dev1.id}"
+output "subnet" {
+  value       = "${aws_subnet.dev1.id}"
 }
-
-output "Subnet2" {
-  value = "${aws_subnet.dev2.id}"
+output "subnet2" {
+  value       = "${aws_subnet.dev2.id}"
 }
-
-output "Subnet3" {
-  value = "${aws_subnet.dev3.id}"
+output "subnet3" {
+  value       = "${aws_subnet.dev3.id}"
 }
-
+output "private subnet1" {
+  value       = "${aws_subnet.dev1.id}"
+}
 output "private subnet2" {
-  value = "${aws_vpc.dev_private1.id}"
+  value       = "${aws_subnet.dev2.id}"
 }
-
-output "private subnet2" {
-  value = "${aws_vpc.dev_private2.id}"
+output "private subnet3" {
+  value       = "${aws_subnet.dev3.id}"
 }
-
-output "private subnet2" {
-  value = "${aws_vpc.dev_private3.id}"
-}
-
 output "IGW" {
-  value = "${aws_internet_gateway.dev.id}"
+  value       = "${aws_internet_gateway.dev.id}"
 }
