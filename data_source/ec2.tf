0ac97433f6cca81fe38b1ad4155dad091d31ec57
Cloud User <centos@ip-172-31-87-249.ec2.internal>
2020-03-13 01:55:39 +0000

    null resurce added

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -1,39 +1,43 @@
 provider "aws" {
   region = "us-west-2"
 }
-# Get ubuntu AMI
+
+# Gets Ubuntu AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
+
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
   }
+
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+
   owners = ["099720109477"] # Canonical
 }
+
 output "UBUNTU_AMI_ID" {
-  value       = "${data.aws_ami.ubuntu.id}"
+  value = "${data.aws_ami.ubuntu.id}"
 }
 
-
-# Get  centosOS AMI
+# Gets Centos AMI
 data "aws_ami" "centos" {
   most_recent = true
+
   filter {
     name   = "name"
     values = ["CentOS Linux 7 x86_64 HVM EBS *"]
   }
+
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+
   owners = ["679593333241"] # Canonical
-}
-output "CENTOS_AMI_ID" {
-  value       = "${data.aws_ami.centos.id}"
 }
 
 resource "aws_key_pair" "provisioner" {
@@ -48,18 +52,21 @@ resource "aws_instance" "web" {
 
   provisioner "file" {
     connection {
-    type     = "ssh"
-    user     = "ubuntu"
-    password = "${file("~/.ssh/id_rsa")}"
-    host     = "${self.public_ip}"
-   }
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = "${file("~/.ssh/id_rsa")}"
+      host        = "${self.public_ip}"
+    }
 
     source      = "test"
     destination = "/tmp/"
   }
 
-
   tags = {
     Name = "HelloWorld"
   }
+}
+
+output "CENTOS_AMI_ID" {
+  value = "${data.aws_ami.centos.id}"
 }
